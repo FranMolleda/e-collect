83c265ae52a1d26c2957e7883aabf82aaf27371e
Fran <fmjerez@icloud.com>
2020-04-07 20:39:58 +0200

    Working in home page

--- front/starter-code/src/App.js
+++ front/starter-code/src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "../public/styles/App.css";
+import Footer from "./components/Layout/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LayoutNavbar from "./components/Layout/Navbar";
+import Home from "./pages/home/Home";
 
 export const App = () => {
-  return <div>e-collect running</div>;
+  return (
+    <>
+      {/* <LayoutNavbar /> */}
+      <Home />
+      <Footer />
+    </>
+  );
 };
---
+++ front/starter-code/src/components/Layout/Navbar/styleNavbar.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const NavbarContainer = styled.div`
+  .container-navbar {
+    background: linear-gradient(to right, #b1e2eb 0%, #1a5059 100%);
+    color: rgba(255, 255, 255, 1);
+    position: absolute;
+    top: 0;
+    width: 100%;
+    padding: 10px 30px;
+  }
+  .mr-nav-left {
+    margin-left: 100px;
+  }
+`;
+export default NavbarContainer;

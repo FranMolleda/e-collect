b10cd8ca00f5775ff7c9a440408e4183523cc3a1
Fran <fmjerez@icloud.com>
2020-04-20 11:48:39 +0200

    Heroku again

--- front/dist/src.a2b27638.js
+++ front/dist/src.a2b27638.js
@@ -72230,7 +72230,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51901" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52758" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
